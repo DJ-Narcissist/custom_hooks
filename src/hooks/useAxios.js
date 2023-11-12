f1de1b5cdff920af0ee92fb433a3e443bc8b1c6d
Christian Wise <djnarcissist@Christians-MacBook-Pro.local>
2023-11-12 17:50:58 -0500

    useAxios and playing card list

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,8 +1,21 @@
+import axios from "axios";
+import { url } from "inspector";
 import  {useState} from "react";
 
 
 const useAxios = () => {
+    const [data, setData] = useState();
 
+    const addData = async () => {
+        try {
+            const response = await axios.get(url);
+            setData((prevData) => [...prevData, response.data]);
+        }   catch (error) {
+            console.error("Error fetching data",error);
+        }
+    }
+
+    return [data,addData];
 }
 
 export default useAxios;
